e56a78548b3e61c5058b410ea82f9b19db2f87af
Admin <timur>
2023-10-03 23:06:45 +0300

    add new example

--- README.md
+++ README.md
@@ -5,15 +5,61 @@
 *Share numpy arrays between processes*
 <br/>
 
-**example:**
+**how use shared array:**
 ```python
-import winsharedarray as sa
+#process 1
+
+import numpysharedarray as nps
 import numpy as np
 
 arr = np.zeros((5,5))
 
-sa.create_mem_sh("shm_mem_npy", arr)
-array_attached = sa.attach_mem_sh("shm_mem_npy")
+#this function create new array and copy in shared memory
+nps.create_mem_sh("shm_mem_npy", arr)
+#after create let's attach to shared array
+array_attached = nps.attach_mem_sh("shm_mem_npy")
+#you can fulfill any numpy operation
 array_attached[:3,:1] = 1
+array_attached[:3,0] = np.sin(array_attached[:3,0])
+array_attached[:1,1:] += 12
+
+#process 2
+import numpysharedarray as nps
+import numpy as np
+
+#shared array has already been created so just attach
+array_attached = nps.attach_mem_sh("shm_mem_npy")
+# array([[ 0.84147098, 12.        , 12.        , 12.        , 12.        ],
+#        [ 0.84147098,  0.        ,  0.        ,  0.        ,  0.        ],
+#        [ 0.84147098,  0.        ,  0.        ,  0.        ,  0.        ],
+#        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ],
+#        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ]])
+print(array_attached)
+
+```
 
 
+**how use shared array and mutex:**
+```python
+import numpysharedarray as nps
+import numpy as np
+
+from multiprocessing import Pool
+
+def mutex_test(num):
+    mutx = nps.open_mutex("mutex_name")
+    cntr = nps.attach_mem_sh("counter_name")
+    #we protect the array using a mutex
+    nps.capture_mutex(mutx)
+    cntr[0][0] += 1
+    nps.release_mutex(mutx)
+
+counter_array = np.zeros((1,1))
+nps.create_mem_sh("counter_name", counter_array)
+mutx = nps.create_mutex("mutex_name")
+
+p = Pool(5)
+p.map(mutex_test, range(5000))
+attach_array = nps.attach_mem_sh("counter_name")
+print(attach_array)
+```
--- example.py
+++ example.py
@@ -1,95 +1,94 @@
 from multiprocessing import Pool
-import winsharedarray as sa
+import numpysharedarray as nps
 import numpy as np
 
 def mutex_test(num):
-	arr = np.zeros((1,1))
-	mutx = sa.open_mutex("sem_cntr_ex")
-	cntr = sa.attach_mem_sh("/shm_mem_npy_counter")
+	mutx = nps.open_mutex("sem_cntr_ex")
+	cntr = nps.attach_mem_sh("/shm_mem_npy_counter")
 	for ix in range(num):
-		sa.capture_mutex(mutx)
+		nps.capture_mutex(mutx)
 		cntr[0][0] += 1
-		sa.release_mutex(mutx)
+		nps.release_mutex(mutx)
 
 if __name__ == '__main__':
-	print(dir(sa))
+	print(dir(nps))
 
 	## TEST 1
 	print("TEST 1")
 
 	arr = np.zeros((5,5))
 
-	print(sa.check_mem_sh("/shm_mem_npy_test_1f"))
-	sa.create_mem_sh("/shm_mem_npy_test_1f", arr)
+	print(nps.check_mem_sh("/shm_mem_npy_test_1f"))
+	nps.create_mem_sh("/shm_mem_npy_test_1f", arr)
 
 	try:
-		array_attached = sa.attach_mem_sh("/shm_mem_npy_test_12f")
-	except RuntimeError:
-		array_attached = sa.attach_mem_sh("/shm_mem_npy_test_1f")	
+		array_attached = nps.attach_mem_sh("/shm_mem_npy_test_12f")
+	except:
+		array_attached = nps.attach_mem_sh("/shm_mem_npy_test_1f")	
 
 	array_attached[:3,:1] = 1
 	print(array_attached)
-	print(sa.delete_mem_sh("/shm_mem_npy_test_1f"))
-	print(sa.check_mem_sh("/shm_mem_npy_test_1f"))
+	print(nps.delete_mem_sh("/shm_mem_npy_test_1f"))
+	print(nps.check_mem_sh("/shm_mem_npy_test_1f"))
 
 	## TEST 2
 	print("TEST 2")
 
 	arr = np.zeros((1081, 1920, 24))
 
-	sa.create_mem_sh("/shm_mem_npy_test_2", arr)
+	nps.create_mem_sh("/shm_mem_npy_test_2", arr)
 
 	try:
-		array_attached = sa.attach_mem_sh("/shm_mem_npy_test_12")
-	except RuntimeError:
-		array_attached = sa.attach_mem_sh("/shm_mem_npy_test_2")	
+		array_attached = nps.attach_mem_sh("/shm_mem_npy_test_12")
+	except:
+		array_attached = nps.attach_mem_sh("/shm_mem_npy_test_2")	
 
 	array_attached[:30,:300] = 88.
 	print(arr.mean())
 	print(array_attached[:,:,:1].mean())
-	sa.delete_mem_sh("/shm_mem_npy_test_2")
+	nps.delete_mem_sh("/shm_mem_npy_test_2")
 	## TEST 3
 	print("TEST 3")
 
 	arr = np.zeros((4,4)).astype(np.float64)
-	sa.create_mem_sh("/shm_mem_npy_test_3", arr)
-	array_attached = sa.attach_mem_sh("/shm_mem_npy_test_3")
+	nps.create_mem_sh("/shm_mem_npy_test_3", arr)
+	array_attached = nps.attach_mem_sh("/shm_mem_npy_test_3")
 	# do something
-	array_attached = sa.attach_mem_sh("/shm_mem_npy_test_3")
+	array_attached = nps.attach_mem_sh("/shm_mem_npy_test_3")
 	#import time
 	#time.sleep(10)
-	#print(sa.delete_mem_sh("/shm_mem_npy_test_3"))
+	#print(nps.delete_mem_sh("/shm_mem_npy_test_3"))
 
-	array_attached = sa.attach_mem_sh("/shm_mem_npy_test_3")	
+	array_attached = nps.attach_mem_sh("/shm_mem_npy_test_3")	
 
 	array_attached[:3,:1] = 1
 	print(array_attached)
 
 	print(
-		sa.check_mem_sh("/shm_mem_npy_test_3"),
-		sa.check_mem_sh("/shm_mem_npy_test_55")
+		nps.check_mem_sh("/shm_mem_npy_test_3"),
+		nps.check_mem_sh("/shm_mem_npy_test_55")
 	)
-	sa.delete_mem_sh("/shm_mem_npy_test_3")
-	sa.create_mem_sh("shm_mem_npy_test_55", arr)
+	nps.delete_mem_sh("/shm_mem_npy_test_3")
+	nps.create_mem_sh("/shm_mem_npy_test_55", arr)
 	print(
-		sa.check_mem_sh("/shm_mem_npy_test_3"),
-		sa.check_mem_sh("/shm_mem_npy_test_55")
+		nps.check_mem_sh("/shm_mem_npy_test_3"),
+		nps.check_mem_sh("/shm_mem_npy_test_55")
 	)
-	sa.delete_mem_sh("/shm_mem_npy_test_55")
+	nps.delete_mem_sh("/shm_mem_npy_test_55")
 	## TEST 4
 	print("TEST 4")
 
-	mutx = sa.create_mutex("sem_cntr_ex")
-	sa.release_mutex(mutx)
+	mutx = nps.create_mutex("sem_cntr_ex")
+	nps.release_mutex(mutx)
 
 	arr = np.zeros((1,1))
-	sa.create_mem_sh("/shm_mem_npy_counter", arr)
+	nps.create_mem_sh("/shm_mem_npy_counter", arr)
 
 	p = Pool(5)
-	p.map(mutex_test, [10000, 20000, 30000])
+	p.map(mutex_test, list(range(1000)))
 
-	cntr = sa.attach_mem_sh("/shm_mem_npy_counter")
-	print(cntr[0][0]) # == 10000+20000+30000=60000
-	sa.delete_mem_sh("/shm_mem_npy_counter")
-	print("GetLastError() = ", sa.get_last_error())
+	cntr = nps.attach_mem_sh("/shm_mem_npy_counter")
+	print(cntr[0][0]) # 499500
+	nps.delete_mem_sh("/shm_mem_npy_counter")
+	print("GetLastError() = ", nps.get_last_error())
 
